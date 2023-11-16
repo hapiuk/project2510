1c889c3802fb99a1af02dd277c0da24e94f37463
hapiuk <aaron.gomm@outlook.com>
2023-11-16 19:56:26 +0000

    Contracts
    Contracts and the details page have been implemented. Will need further tuning, however we can now proceed with the jobs functionality and pad the rest out at a later date.

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ import io
 import zipfile
 import sqlite3
 import shutil
-from datetime import datetime
+from datetime import datetime, timedelta
+import calendar
 
 app = Flask(__name__)
 app.debug = True
@@ -21,6 +22,15 @@ logging.basicConfig(level=logging.INFO)
 
 # Database Configuration
 DATABASE = './static/projectdb.db'
+
+def get_all_clients():
+    conn = get_db()
+    cursor = conn.cursor()
+    query = '''SELECT * FROM clients'''  # Adjust the SQL query as needed
+    cursor.execute(query)
+    clients = cursor.fetchall()
+    conn.close()
+    return clients
 
 def get_db():
     conn = sqlite3.connect(DATABASE)
@@ -121,7 +131,8 @@ def generate_equipment_number():
 def get_all_clients():
     conn = get_db()
     cursor = conn.cursor()
-    cursor.execute('SELECT id, client_name FROM clients')
+    query = '''SELECT * FROM clients'''  # Adjust the SQL query as needed
+    cursor.execute(query)
     clients = cursor.fetchall()
     conn.close()
     return clients
@@ -131,6 +142,8 @@ def root():
     return render_template('index.html')
 
 ########################################################################################################################################### Client Functions
+
+
 @app.route('/clients')
 def clients():
     search_query = request.args.get('search', '', type=str)
@@ -512,24 +525,40 @@ def equipment_search():
     return jsonify(equipment)
 
 ###################################################################################################################################### Contract Functions
-@app.route('/get-equipment-for-client/<account_number>')
-def get_equipment_for_client(account_number):
-    print("Received Account Number:", account_number)  # Debugging line
-    equipment = fetch_equipment_for_client(account_number)
-    print("Returning Equipment:", equipment)  # Debugging line
-    return jsonify([dict(eq) for eq in equipment])
 
-def fetch_equipment_for_client(account_number):
+def get_contract_data(contract_id):
     conn = get_db()
     cursor = conn.cursor()
     cursor.execute('''
-        SELECT id, equipment_number, equipment_type 
-        FROM equipment 
-        WHERE client_account_number = ?
+        SELECT contracts.*, clients.client_name
+        FROM contracts
+        INNER JOIN clients ON contracts.client_account_number = clients.account_number
+        WHERE contracts.id = ?
+    ''', (contract_id,))
+    contract_data = cursor.fetchone()
+    conn.close()
+    return dict(contract_data) if contract_data else None
+
+# Function to get equipment for a client based on account number
+def fetch_equipment_for_client(account_number):
+    print("Receiving Equipment Data")
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+        SELECT * FROM equipment WHERE client_account_number = ?
     ''', (account_number,))
     equipment = cursor.fetchall()
     conn.close()
+    print("Received Equipment Data")
     return equipment
+
+@app.route('/get-equipment-for-client/<account_number>')
+def get_equipment_for_client(account_number):
+    print("Received Account Number:", account_number)  # Debugging line
+    equipment_rows = fetch_equipment_for_client(account_number)
+    equipment = [dict(row) for row in equipment_rows]  # Convert rows to dictionaries
+    print("Returning Equipment:", equipment)  # Debugging line
+    return jsonify(equipment)  # Return equipment data as JSON
 
 @app.route('/contracts')
 def contracts():
@@ -544,41 +573,75 @@ def get_all_contracts():
     cursor.execute('''
         SELECT contracts.*, clients.client_name
         FROM contracts
-        INNER JOIN clients ON contracts.client_account_number = clients.account_number
+        JOIN clients ON contracts.client_account_number = clients.account_number
     ''')
     contracts = cursor.fetchall()
     conn.close()
-    return contracts
+    return [dict(contract) for contract in contracts]
+
+@app.route('/contract-details/<contract_id>')
+def contract_details(contract_id):
+    contract_data = get_contract_data(contract_id)  # This should fetch contract and associated client data
+    if contract_data:
+        print(contract_data)  # Debug print
+        return render_template('contract_details.html', contract=contract_data)
+    else:
+        flash('Contract not found', 'error')
+        return redirect(url_for('contracts'))
 
 
 @app.route('/create-contract', methods=['POST'])
 def create_contract():
     if request.method == 'POST':
         client_account_number = request.form['client_account_number']
-        equipment_ids = request.form.getlist('equipment[]')  # Handle multiple equipment selections
+        equipment_ids = request.form.getlist('equipment[]')
         job_type = request.form['job_type']
-        start_date = request.form['start_date']
-        end_date = request.form['end_date']
-        renewal_date = calculate_renewal_date(end_date)  # Implement this function based on your logic
+        start_date_str = request.form['start_date']
+
+        # Convert start_date string to datetime object
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+
+        # Calculate the end date (12 months from the start date)
+        end_date = add_months_to_date(start_date, 12)
+
+        # Calculate renewal date (11 months from the start date)
+        renewal_date = add_months_to_date(start_date, 11)
+
         contract_charge = request.form['contract_charge']
         billing_cycle = request.form['billing_cycle']
 
         conn = get_db()
         cursor = conn.cursor()
         cursor.execute('''INSERT INTO contracts (
-                              client_account_number, equipment_ids, job_type, start_date, end_date,
-                              renewal_date, contract_charge, billing_cycle
+                          client_account_number, equipment_ids, job_type, start_date, end_date,
+                          renewal_date, contract_charge, billing_cycle
                           ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', 
-                       (client_account_number, ','.join(equipment_ids), job_type, start_date, 
-                        end_date, renewal_date, contract_charge, billing_cycle))
+                       (client_account_number, ','.join(equipment_ids), job_type, start_date_str, 
+                        end_date.strftime('%Y-%m-%d'), renewal_date.strftime('%Y-%m-%d'), contract_charge, billing_cycle))
         conn.commit()
         conn.close()
 
         return redirect(url_for('contracts'))
 
+def add_months_to_date(original_date, months):
+    # Add the specified number of months to the original date
+    month = original_date.month - 1 + months
+    year = original_date.year + month // 12
+    month = month % 12 + 1
+    day = min(original_date.day, calendar.monthrange(year, month)[1])
+    return datetime(year, month, day)
+
+def add_months(source_date, months):
+    month = source_date.month - 1 + months
+    year = source_date.year + month // 12
+    month = month % 12 + 1
+    day = min(source_date.day, calendar.monthrange(year, month)[1])
+    return datetime(year, month, day)
+
 create_equipment_table()
 create_clients_table()
-create_contracts_table() 
+create_contracts_table()
+
 
 if __name__ == "__main__":
     import socket
